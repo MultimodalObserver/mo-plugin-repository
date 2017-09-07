48418e997c06985351a69abcd472c12884105ec7
Felo Vilches <felovilches@gmail.com>
2017-09-07 20:44:40 -0300

    una manera poco elegante de colocar una "pagina inicial"

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/', to: proc { [200, {}, ['MO Plugin Repository API']] }
+
   resources :users, only: [:index, :show]
 
   get '/users/me', to: 'users#me'
