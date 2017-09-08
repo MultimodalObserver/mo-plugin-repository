1204a9f4c90e2ad088a61b40f0d328a2ab7bac8f
Felo Vilches <felovilches@gmail.com>
2017-09-08 00:20:13 -0300

    refactor simple url

--- app/models/concerns/simple_url.rb
+++ app/models/concerns/simple_url.rb
@@ -2,13 +2,28 @@ require 'uri'
 
 class SimpleUrl
 
-  attr_accessor :scheme, :full_url, :guess_host, :full_url_default_scheme, :path_array
+  # http, https, ftp, etc
+  attr_accessor :scheme
+
+  # given URL (passed to the constructor)
+  attr_accessor :full_url
+
+  # http://www.google.com ---> "google"
+  attr_accessor :guess_host
+
+  # URL with a default scheme (if the URL originally didn't have a
+  # scheme, and the default was set to be FTP, then this URL starts with ftp://)
+  attr_accessor :full_url_default_scheme
+
+  # If the URL was http://youtube.com/aaa/bbb/ccc
+  # then this array contains ["aaa", "bbb", "ccc"]
+  attr_accessor :path_array
 
   def initialize(string_url, default_scheme = 'http')
     default_scheme.downcase! if !default_scheme.nil?
     @full_url = string_url.strip
-    @full_url_default_scheme = @full_url
-    arr = parse(string_url, default_scheme)
+    @full_url_default_scheme = set_default_scheme @full_url, default_scheme
+    arr = parse @full_url_default_scheme
     @scheme = arr[0].downcase
     body = arr[3]
     @path_array = get_path_array arr[6]
@@ -17,12 +32,21 @@ class SimpleUrl
 
   private
 
+  #
+  # Gets the URL path, for instance
+  # http://youtube.com/aaa/bbb/ccc
+  # returns
+  # ["aaa", "bbb", "ccc"]
+  #
   def get_path_array(path_parts)
     return [] if path_parts.nil?
     path_parts.split('/').reject { |x| x.empty? }
   end
 
 
+  #
+  # Gets the URL host using some heuristics
+  #
   def get_host(body)
     return "" if body.nil?
 
@@ -40,15 +64,26 @@ class SimpleUrl
       return parts[(parts.count - 1) / 2]
     end
     return parts[parts.count / 2]
-
   end
 
-  def parse(url, default_scheme)
+
+  #
+  # Adds a default scheme if the URL didn't have one
+  #
+  def set_default_scheme(url, default_scheme)
     array = url.scan(URI.regexp)
     if array.empty?
-      @full_url_default_scheme = default_scheme + "://" + url
-      array = @full_url_default_scheme.scan(URI.regexp)
+      url = default_scheme + "://" + url
     end
+    return url
+  end
+
+
+  #
+  # Breaks the URL and returns each part
+  #
+  def parse(url)
+    array = url.scan(URI.regexp)
     return array[0]
   end
 end
