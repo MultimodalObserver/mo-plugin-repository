f2289a44fccaa8f8e7484063f226b0f677bae2e8
Felo Vilches <felovilches@gmail.com>
2017-09-07 23:52:46 -0300

    rspec shit commit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
-    sequence :email do |n|
-      "felipe#{n}@mail.jp"
-    end
+
+    sequence :email { |n| "felipe#{n}@mail.jp" }
 
     password  "123456789"
     password_confirmation  "123456789"
