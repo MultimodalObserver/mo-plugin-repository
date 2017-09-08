920901f871bdb2bdae393b5b34878eed3792c03f
Felo Vilches <felovilches@gmail.com>
2017-09-07 23:48:25 -0300

    semaphoreci bug 3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    sequence :email { |n| "felipe#{n}@mail.jp" }
+    
+    sequence :email do |n|
+      "felipe#{n}@mail.jp"
+    end
+
     password  "123456789"
     password_confirmation  "123456789"
 
