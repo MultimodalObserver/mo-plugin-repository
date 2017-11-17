3070751983a746bd93c3f2a57afa37dbb01ae953
Felo Vilches <felovilches@gmail.com>
2017-11-16 22:07:13 -0300

    arregle un script

--- reset_heroku.sh
+++ reset_heroku.sh
@@ -1,6 +1,6 @@
 heroku pg:reset database --confirm mo-plugin-repository
-heroku run rake reset_sequences
+heroku run rake database:reset_sequences
 heroku run rake db:migrate
 heroku run rake db:seed
-heroku restart --app mo-repository-plugin
+heroku restart --app mo-plugin-repository
 
