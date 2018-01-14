65dda6fdd5ea1bf87bd9dc3922e659bd5c32aab8
Felo Vilches <felovilches@gmail.com>
2018-01-14 16:52:16 -0300

    api key captcha pasada a secrets y env

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,9 @@
 # config/initializers/recaptcha.rb
 Recaptcha.configure do |config|
-  config.site_key  = '6LcEiUAUAAAAAPnNOCGhm6rphB098cCZi9zCTiom'
-  config.secret_key = '6LcEiUAUAAAAAKNamOD1QktI_pQ0UQuFw50mQiGD'
+
+  config.site_key  = Rails.application.secrets.recaptcha_site_key
+  config.secret_key = Rails.application.secrets.recaptcha_secret_key
+
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
